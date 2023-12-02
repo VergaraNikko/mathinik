0c8173f3d11399beacbe2901cc42097f9112c379
jclarky0 <120705960+jclarky0@users.noreply.github.com>
2023-12-02 16:20:57 +0800

    UserService.java

--- mathinik/UserService.java
+++ mathinik/UserService.java
@@ -1,15 +1,15 @@
-package com.csit321g2.vergara.Service;
+package com.csit321g2.Olbenario.Service;
 
 import java.util.List;
 
-import com.csit321g2.vergara.Entity.User;
+import com.csit321g2.Olbenario.Entity.User;
 
 public interface UserService {
 	User createUser(User user);
-	User authenticateUser(String username, String password);
     User getUser(int userid);
     User updateUser(int userid, User user);
     void deleteUser(int userid);
 	List<User> getAllUsers();
+	User authenticateUser(String username, String password);
 	
 }
