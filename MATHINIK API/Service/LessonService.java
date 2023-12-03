8ad45b4003274bd3864a64c93e10524240856bd7
jclarky0 <120705960+jclarky0@users.noreply.github.com>
2023-12-03 21:38:30 +0800

    LessonService.java

--- MATHINIK API/Service/LessonService.java
+++ MATHINIK API/Service/LessonService.java
@@ -1,15 +1,15 @@
-package com.csit321g2.vergara.Service;
+package com.csit321g2.Olbenario.Service;
 
-import com.csit321g2.vergara.Entity.LessonEntity;
+import com.csit321g2.Olbenario.Entity.LessonEntity;
 
 import java.util.List;
 
 public interface LessonService {
 	
-    LessonEntity createLesson(int teacherid, String title, String description);
+    LessonEntity createLesson(int userid, String title, String description, String content);
     LessonEntity getLesson(int lessonid);
     List<LessonEntity> getAllLessons();
-    LessonEntity updateLesson(int lessonid, int teacherid, String title, String description);
+    LessonEntity updateLesson(int lessonid, int userid, String title, String description, String content);
     void deleteLesson(int lessonid);
     
 }
